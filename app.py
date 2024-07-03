def2aa5a1828fa342fe9c60a156bc2e5a4d42037
Kingsley <kingbrookeyyahoo.com  git config --global user.name Kingsley>
2024-07-03 15:35:37 +0100

    added app insight config

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from datetime import datetime, timezone
-
 from werkzeug.utils import redirect
 from flask_config import Config
-from data.database import initialise_database, add_order, clear_orders, count_orders, get_orders_to_display, get_queued_count, get_recently_placed_count, get_recently_processed_count
+from data.database import (
+    initialise_database, add_order, clear_orders, count_orders, 
+    get_orders_to_display, get_queued_count, get_recently_placed_count, 
+    get_recently_processed_count
+)
 from scheduled_jobs import initialise_scheduled_jobs
 from products import create_product_download
 import requests
 import logging
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
 
 logging.basicConfig(level=logging.INFO)
+app.logger.addHandler(AzureLogHandler())
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +25,15 @@ app.config.from_object(Config)
 initialise_database(app)
 initialise_scheduled_jobs(app)
 
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string='${{ secrets.APP_INSIGHTS_CONNECTION_STRING }}'),
+    sampler=ProbabilitySampler(rate=1.0),
+)
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
 
 @app.route("/")
 def index():
@@ -25,28 +42,31 @@ def index():
     recently_placed_count = get_recently_placed_count()
     recently_processed_count = get_recently_processed_count()
     scenarios = [
-        { 'display': 'Add broken order', 'value': 'BrokenOrder' },
-        { 'display': 'Monitoring Load', 'value': 'HighLoad' },
-        { 'display': 'Queue Reliability', 'value': 'UnreliableProcessing' },
-        { 'display': 'System Monitoring', 'value': 'VeryHighLoad' },
-        { 'display': 'Reset to initial', 'value': 'Reset' }
+        {'display': 'Add broken order', 'value': 'BrokenOrder'},
+        {'display': 'Monitoring Load', 'value': 'HighLoad'},
+        {'display': 'Queue Reliability', 'value': 'UnreliableProcessing'},
+        {'display': 'System Monitoring', 'value': 'VeryHighLoad'},
+        {'display': 'Reset to initial', 'value': 'Reset'}
     ]
 
     return render_template(
-        "layout.html", orders=orders, queue_count=queue_count, recently_placed_count=recently_placed_count,
-        recently_processed_count=recently_processed_count, scenarios=scenarios
+        "layout.html", 
+        orders=orders, 
+        queue_count=queue_count, 
+        recently_placed_count=recently_placed_count,
+        recently_processed_count=recently_processed_count, 
+        scenarios=scenarios
     )
 
 @app.route("/count")
 def count():
-    return { 'count': count_orders() }
-
+    return {'count': count_orders()}
 
 @app.route("/new", methods=["POST"])
 def new_order():
     product = request.json["product"]
     customer = request.json["customer"]
-    date_placed = request.json["date_placed"] or datetime.now(tz=timezone.utc)
+    date_placed = request.json.get("date_placed", datetime.now(tz=timezone.utc))
     download = create_product_download(product)
     try:
         order = add_order(product, customer, date_placed, None, download)
@@ -54,7 +74,6 @@ def new_order():
         return str(e)
 
     return f"Added: {order}"
-
 
 @app.route("/scenario", methods=["POST"])
 def set_scenario():
@@ -79,4 +98,4 @@ def set_scenario():
     return redirect('/')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='localhost', port=8080, threaded=True)
